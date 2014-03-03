a7431ff405dc3dc41d1ca8cd424c53f62ac4d04f
Hieu Nguyen <hieuk09@gmail.com>
2014-03-03 08:36:19 +0700

    WIP - [f] Save game

---
+++ Assets/Script/SaveFile.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+import System.IO;
+
+var path = 'save.txt'
+
+class SaveFile {
+    function save() {
+        var sw : StreamWriter = new StreamWriter(path);
+        sw.WriteLine("Line to write");
+        sw.WriteLine("Another Line");
+        sw.Flush();
+        sw.Close();    
+    };
+
+    function load() {
+        sr = new File.OpenText(path);
+        input = "";
+
+        while (true) {
+            input = sr.ReadLine();
+            if (input == null) { break; }
+        }
+        sr.Close();
+    };
+}
